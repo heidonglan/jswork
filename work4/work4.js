be7db856321522ed0084f25ef9b87749a773b84e
heidonglan <2414160503@qq.com>
2019-10-12 11:48:49 +0800

    work4

---
+++ work4/work4.js
@@ -0,0 +1,32 @@
+document.write("<p>金字塔</p>")
+let leval = prompt('请设置金字塔的层数')
+leval = parseFloat(leval)&&Number(leval)
+if (isNaN(leval)){
+    alert('金字塔的层数是数字')
+}
+for(let i=1;i<=leval;++1){
+    let blank = leval - i;
+    for(let k=0;k<blank;++k){
+        document.write('&nbsp;');
+    }
+    //打印星星
+    let star = i * 2 - 1
+    for(let j=0;j<star;++j){
+        document.write('*')
+    }
+    //换行
+    document.write('<br>')
+}
+
+let str = '<table border="1">'
+for(let i=1;i<10;++i){
+    str +='<tr>'
+    for(var j=1;j<=i;++j){
+    //拼接单元格
+    str +='<td>' + j + '*' + i + '=' + (j*i)+'</td>'
+}
+str +='</tr>'
+}
+str +='</table>'
+//设置div的html文档内容
+document.getElementById('table').innerHTML=str
