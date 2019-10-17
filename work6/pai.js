afe3796a2953ecf88467d3aedd3b9ab3efa04671
heidonglan <2414160503@qq.com>
2019-10-17 09:59:15 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,31 @@
+
+function caluelate(){
+    let num=document.getElementById('pai').value
+    num=parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('pai').value=pi(num)
+    }
+}
+function factorial(n){
+    let sum=1
+    for(let i=1;i<=sum;++i){
+        str
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum=1
+    for(let i=1){
+        s
+    }
+    return sum
+}
+function pi(num){
+    let sum=0
+    for(let i=0;i<=num;++i){
+        let divisor=factorial()
+        let divisor=oddFactorial()
+    }
+    return sum*2
+}
+caluelate()
