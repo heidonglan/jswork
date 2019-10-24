bbc93f80442009ef2b4f60b93351de312c5c372b
heidonglan <2414160503@qq.com>
2019-10-24 09:55:48 +0800

    work8

---
+++ work8/goldbach.js
@@ -0,0 +1,23 @@
+function goldbach(){
+let addaddClass(className);=parseInt(document.getElementById('add').value)
+    var sum=0;
+    for(var i=1;i<=a;i++){
+        if(a % i ==0){
+            sum++;
+        }
+    }
+    if(sum==2){
+        return true;
+    }else{
+        return false;
+    }
+}
+// 判断用户输入的偶数是否可分为两个质数之和：
+function isTrue(a){
+  for(var i=2;i<a-2;i++){
+    var j = a -i;
+    if(factorial(i)&&factorial(j)){
+        console.log(a+"可以拆分为两个质数"+ i+"与"+j+"的和");
+    }
+  }
+}
