1f7c9736163216d11553ac6b6785675861049cef
heidonglan <2414160503@qq.com>
2019-10-31 21:43:05 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -22,8 +22,14 @@ function monkey(){
     document.getElementById('monkeyking').innerText=monkey[0]
 }
 function stat(){
-    let str=document.getElementById("str").value
-    let obj={}
-    str.split("")
-    document.getElementById('result').innerText=JSON.stringify(obj)
+    let str = document.getElementById("str").value
+    let obj = {}
+    let arr = str.split("")
+    arr = arr.sort()
+    for (let i = 0; i < str.length; ++i){
+        let dong = arr[i]
+        obj[dong] = (obj [dong] + 1) || 1;
+    } 
+
+    document.getElementById('result').innerText = JSON.stringify(obj)
 }
