0911dbd4d2f1b49cc894fa0a1aa3b72e08407ca4
heidonglan <2414160503@qq.com>
2019-10-31 09:54:56 +0800

    work10

---
+++ work10/index.js
@@ -0,0 +1,29 @@
+function monkey(){
+    let total=document.getElementById("monkeytotal").value
+    let kick=document.getElementById("monkeykick").value
+    total=parseInt(total)&&Number(total)
+    kick=parseInt(kick)&&Number(kick)
+    if(isNaN(total)||isNaN(kick)){
+        alert('请输入数字')
+        return
+    }
+    let monkey=[]
+    for(let i=1;i<=total;i++){
+        monkey.push(i)
+    }
+    let i=0
+    while(monkey.length>1){
+        i++;
+        head=monkey.shift()
+        if(i%kick!=0){
+            monkey.push(head);
+        }
+    }
+    document.getElementById('monkeyking').innerText=monkey[0]
+}
+function stat(){
+    let str=document.getElementById("str").value
+    let obj={}
+    str.split("")
+    document.getElementById('result').innerText=JSON.stringify(obj)
+}
