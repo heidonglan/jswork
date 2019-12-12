f32100e26a00c46a3d48e9dd34298103dfb948d0
heidonglan <2414160503@qq.com>
2019-12-12 14:49:07 +0800

    work16

---
+++ work15/index.js
@@ -0,0 +1,21 @@
+ShopCart('cart',[{
+    name:'web前端开发中级 (上册) ',
+    price: 69.0,
+    num: 2
+},
+{
+    name: 'web前端开发中级 (下册) ',
+    price: 69.0,
+    num:2
+},
+{
+    name: 'web前端开发高级 (上册) ',
+    price: 49.0,
+    num:3
+},
+{
+    name: 'web前端开发高级 (下册) ',
+    price: 49.0,
+    num:3
+}
+]);
