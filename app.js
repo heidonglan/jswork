68c272d1dd262d5eaf123875e3f5b4015d77716e
heidonglan <2414160503@qq.com>
2019-12-24 13:01:33 +0800

    work17

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ app.post('/formBuilder', function (req, res) {
     console.log(req.body)
     res.send(req.body)
   })
+  let ajaxData=[]
+  let count=0
+  app.post('/ajax',function(req,res){
+    let sno=req.body.sno
+    let name=req.body.name
+    let content=req.body.content
+    let comment={
+      id:count+1,
+      sno:sno,
+      user:name,
+      time:new Date().toLocaleString(),
+      content:content
+    }
+    console.log(comment)
+    ajaxData.push(comment)
+    count=ajaxData.length
+    res.json(ajaxData)
+  })
+  app.get('/ajax',function(req,res){
+      let page=req.query.page?Math.max(req.query.page,1):1
+      let size=5
+      let maxpage=Math.ceil(ajaxData.length/size)
+      result={data:ajaxData.slice((page-1)*size,page*size),
+      maxPage:maxpage
+    }
+    res.json(result)
+    })
+  
 
 app.listen(8080, () => console.log('node express 服务器已启动，监听端口：8080'))
 
